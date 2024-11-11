554ad6c40311961e3884e8c483ddc50cdcb82c73
Filipe Ferreira <filipeferreira.psid2019@gmail.com>
2024-11-11 15:28:32 -0300

    Função de buscar novos álbuns feita

---
+++ spotify/src/test/java/api_spotify_consumes/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package api_spotify_consumes.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
